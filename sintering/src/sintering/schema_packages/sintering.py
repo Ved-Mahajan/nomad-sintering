2d5a8440615cec002fb564f8667941bef316bb9c
Ved Mahajan <vsmahajan2k@gmail.com>
2025-05-20 14:19:08 +0200

    ruff linting

--- sintering/src/sintering/schema_packages/sintering.py
+++ sintering/src/sintering/schema_packages/sintering.py
@@ -16,23 +16,23 @@
 # limitations under the License.
 #
 
-from nomad.datamodel.data import EntryData
-from nomad.datamodel.metainfo.basesections import Process
-from nomad.datamodel.metainfo.basesections import ProcessStep
-import numpy as np
 from typing import (
     TYPE_CHECKING,
 )
+
+import numpy as np
+from nomad.datamodel.data import (
+    ArchiveSection,
+    EntryData,
+)
+from nomad.datamodel.metainfo.basesections import Process, ProcessStep
 from nomad.metainfo import (
     Package,
     Quantity,
-    SubSection,
     Section,
+    SubSection,
 )
-from nomad.datamodel.data import (
-    EntryData,
-    ArchiveSection,
-)
+
 if TYPE_CHECKING:
     from nomad.datamodel.datamodel import (
         EntryArchive,
